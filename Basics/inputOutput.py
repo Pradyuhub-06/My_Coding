b2b33dac3485c79d5db4bcd831097380b2c12204
Pradyuhub-06 <naga.kanna06@gmail.com>
2025-08-02 12:09:27 +0530

    Input and Output

---
+++ Basics/inputOutput.py
@@ -0,0 +1,7 @@
+class Solution():
+    def numberPrint(self):
+        number = int(input("Please enter a number: "))
+        print(number, "is the number you entered")
+
+Answer = Solution()
+Answer.numberPrint()
